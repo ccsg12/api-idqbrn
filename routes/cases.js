455e7166e28f872b68b3bc26af875830a811045d
Vinicius F L Moraes <54691775+vmoraesfl@users.noreply.github.com>
2022-06-07 02:24:38 -0300

    Update cases.js

--- routes/cases.js
+++ routes/cases.js
@@ -4,6 +4,12 @@ const router = express.Router();
 const CaseController = require("../controllers/CaseController");
 const caseController = new CaseController();
 
+router.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000" ); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 router.get("/", caseController.list);
 router.get("/:doencaId", caseController.doenca_case);
 router.post("/", caseController.create);
